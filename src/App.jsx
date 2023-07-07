68a017e9858fdb86d4334dbc32345dacf54d1437
Anitor <anitorabraham@gmail.com>
2023-07-07 11:04:25 +0100

    created the scafolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar, Footer } from "./components";
+import { Analysis, Dashboard, Profile, Template } from "./Pages";
+
 function App() {
   return (
-    <div className="w-full">
-      <h1 className="text-purple_color">We are the world</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/analysis" element={<Analysis />}></Route>
+        <Route path="/template" element={<Template />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/index.jsx
@@ -0,0 +1,6 @@
+import Dashboard from "./Dashboard";
+import Template from "./Template";
+import Profile from "./Profile";
+import Analysis from "./Analysis";
+
+export { Dashboard, Template, Profile, Analysis };
